bfeb4330044886d5ceae2e72f389c35b80c29738
Nigel-Ho23 <nigelho23@gmail.com>
2025-12-01 21:07:17 +0800

    new generalized filter_cellchat function

--- R/filter_cellchat.R
+++ R/filter_cellchat.R
@@ -1,9 +1,10 @@
 #' Subsetting CellChat object to contain interactions significant in both CCC tools
 #'
 #' @param cellchat CellChat object obtained from run_cellchat() on a Seurat object.
-#' @param combine.sig Output data from crosscheck() result element.
+#' @param crosscheck_res Output data from crosscheck() function.
+#' @param category Mandatory character value of the type of interaction to filter the cellchat object.
 #'
-#' @returns A new CellChat object that contains only interactions significant in both CellChat and CellPhoneDB.
+#' @returns A new CellChat object that interactions filtered to your interest (default: significant in both CellChat and CellPhoneDB).
 #' @export
 #'
 #' @examples
@@ -13,37 +14,40 @@
 #' cellchat <- run_cellchat(seurat.obj, ...)
 #'
 #' combine <- crosscheck(cellchat = cellchat, cellphonedb = cpdb)
-#' 
-#' cellchat.new <- filter_cellchat(cellchat = cellchat, combine.sig = combine$result)
+#'
+#' cellchat.new <- filter_cellchat(cellchat = cellchat, crosscheck_res = crosscheck)
 #' }
 
 
-filter_cellchat <- function(cellchat, combine.sig) {
+filter_cellchat <- function(cellchat, crosscheck_res, category = c("Sig_Both", "Sig_CellChat_Not_Found_CellPhoneDB", "Sig_CellPhoneDB_Not_Found_CellChat",
+                                                                   "Sig_CellChat_Not_Sig_CellPhoneDB", "Sig_CellPhoneDB_Not_Sig_CellChat")) {
   if (! is.data.frame(pull_netslot(cellchat))) {
     stop("Input CellChat result is not a dataframe. Perform run_cellchat() on your Seurat object and set cellchat as your output CellChat object.")
-  } else if (! is.data.frame(combine.sig)) {
-    stop("Input combine.sig is not a dataframe. Run crosscheck() on your CellChat object and CellPhoneDB output list and extract the result element.")
+  } else if (! is.data.frame(crosscheck_res$result)) {
+    stop("Input crosscheck_res is not a list. Run crosscheck() on your CellChat object and CellPhoneDB output list and extract the result element.")
   }
-  
-  rel.int <- combine.sig %>%
-    filter(confidence == "Sig_Both") %>%
+
+  category <- match.arg(category)
+
+  to_keep <- crosscheck_res$result %>%
+    filter(category == category) %>%
     pull(interaction_name) %>%
     unique()
-  
+
   cellchat.new <- cellchat
   cellchat.new@net$prob <- cellchat@net$prob[,, rel.int]
   cellchat.new@net$pval <- cellchat@net$pval[,, rel.int]
   cellchat.new@LR$LRsig <- cellchat@LR$LRsig %>%
     filter(interaction_name %in% rel.int)
-  
+
   cellchat.new <- computeCommunProbPathway(cellchat.new)
   cellchat.new <- aggregateNet(cellchat.new)
   cellchat.new <- netAnalysis_computeCentrality(cellchat.new, slot.name = "netP")
-  
-  cat("Created CellChat object with only interactions significant in both CellChat and CellPhoneDB.")
-  
+
+  cat("Filtered CellChat object to only interactions in category:", category, "\n")
+
   return(cellchat.new)
-  
+
 }
 
 
